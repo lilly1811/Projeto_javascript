3800c262135e92be0addb1e08cd2006ed78ccc75
lilly1811 <00001124575637sp@al.educacao.sp.gov.br>
2025-08-21 14:29:09 -0300

    Criando evento de clique no botão login

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const btnLogin = document.querySelector("#btn-login");
+const btnRegister = document.querySelector("#btn-register");
+
+btnLogin.addEventListener("click", (e) => {
+    console.log("Cliquei no botão login");
+    
+})
